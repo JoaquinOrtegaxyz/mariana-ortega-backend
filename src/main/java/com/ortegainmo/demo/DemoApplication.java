74ec5a65a3b7f5bd02b6ceacbaa910d77d06676d
Joaquin <joaco.rodriguezortega@gmail.com>
2026-06-01 03:39:01 -0300

    some fix

--- src/main/java/com/ortegainmo/demo/DemoApplication.java
+++ src/main/java/com/ortegainmo/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.ortegainmo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 import java.util.TimeZone;
 
