3adff6fce994d93fec2a34e6eea1a81389406724
Joaquin <joaco.rodriguezortega@gmail.com>
2026-06-02 00:56:48 -0300

    backend finished

--- src/main/java/com/ortegainmo/demo/security/SecurityConfig.java
+++ src/main/java/com/ortegainmo/demo/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/properties/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/images/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider)
