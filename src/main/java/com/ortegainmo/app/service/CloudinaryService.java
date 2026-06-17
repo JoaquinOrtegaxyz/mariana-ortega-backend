8cf22f16a884d1872dcdeb258912de0326fbcb25
Joaquin <joaco.rodriguezortega@gmail.com>
2026-06-17 17:33:45 -0300

    CloudinaryService create

---
+++ src/main/java/com/ortegainmo/app/service/CloudinaryService.java
@@ -0,0 +1,39 @@
+package com.ortegainmo.app.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    // Spring Boot inyecta los datos de tu application.properties acá
+    public CloudinaryService(
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret) {
+
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret));
+    }
+
+    // Método para subir la foto y que nos devuelva la URL
+    public String uploadFile(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        return uploadResult.get("url").toString();
+    }
+
+    // Opcional: Método para borrar la foto de la nube si eliminás la propiedad
+    public Map deleteFile(String id) throws IOException {
+        return cloudinary.uploader().destroy(id, ObjectUtils.emptyMap());
+    }
+}
