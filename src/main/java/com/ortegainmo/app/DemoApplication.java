472bcdfa1dd29e085c49170bec8fc86d50f050f6
Joaquin <joaco.rodriguezortega@gmail.com>
2026-07-06 03:42:08 -0300

    .

--- src/main/java/com/ortegainmo/app/DemoApplication.java
+++ src/main/java/com/ortegainmo/app/DemoApplication.java
@@ -1,16 +1,40 @@
 package com.ortegainmo.app;
 
+import com.ortegainmo.app.security.user.User;
+import com.ortegainmo.app.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.TimeZone;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.count() == 0) {
+                User admin = new User();
+
+                admin.setEmail("mortegainmo@gmail.com");
+
+                admin.setPassword(passwordEncoder.encode("Mariana123!"));
+
+
+                userRepository.save(admin);
+
+                User admin2 = new User();
+
+                admin2.setEmail("alfredofarisanonec@gmail.com");
+
+                admin2.setPassword(passwordEncoder.encode("Yamil123!"));
+                userRepository.save(admin2);
+            }
+        };
+    }
 }
